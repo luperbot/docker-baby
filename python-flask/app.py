fe01a00b8f6804cb12d80e32f29e66407421e5e1
Claudia Mane <c@luperlabs.com>
2016-05-20 12:08:07 -0700

    Added rabbitmq to docker-compose. Index page of flask-app sends a sample message to rabbitmq to show off how links work.

--- python-flask/app.py
+++ python-flask/app.py
@@ -1,9 +1,25 @@
+#!/usr/bin/env python
 from flask import Flask
+import pika
+
+QUEUE_NAME = 'baby-queue'
+RMQ_HOST = 'rabbitmq'
+
 app = Flask(__name__)
 
+
+def send_message(message):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(RMQ_HOST))
+    channel = connection.channel()
+    channel.queue_declare(queue=QUEUE_NAME)
+    channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=message)
+    connection.close()
+
 @app.route('/')
-def hello_world():
-    return 'Hello from python-flask!'
+def index():
+    message = 'Visitor came to your website.'
+    send_message(message)
+    return 'Sent a message to rabbitmq: {}'.format(message)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
