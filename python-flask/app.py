dcb31f51158946e4647ececba9f74df662e9dde2
Claudia Mane <c@luperlabs.com>
2016-05-20 12:59:51 -0700

    Added message inbox for flask app.

--- python-flask/app.py
+++ python-flask/app.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import datetime
+
 from flask import Flask
 import pika
 
@@ -17,9 +19,28 @@ def send_message(message):
 
 @app.route('/')
 def index():
-    message = 'Visitor came to your website.'
+    now = datetime.datetime.now()
+    message = 'Visitor came to your website @ {}.'.format(now)
     send_message(message)
     return 'Sent a message to rabbitmq: {}'.format(message)
+
+def get_messages():
+    messages = []
+    connection = pika.BlockingConnection(pika.ConnectionParameters(RMQ_HOST))
+    channel = connection.channel()
+    while True:
+        method_frame, header_frame, body = channel.basic_get(QUEUE_NAME)
+        if not method_frame:
+            connection.close()
+            return messages
+        messages.append(body)
+        channel.basic_ack(method_frame.delivery_tag)
+
+@app.route('/inbox')
+def inbox():
+    messages = get_messages()
+    return 'You have the following messages: {}'.format(messages)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
